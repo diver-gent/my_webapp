1eeea327488e444d019469aca8bab708fa41002d
townsley <williamwtownsleyiii@gmail.com>
2016-04-10 10:27:44 -0700

    reorg

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 
 setup(
     name='mypy_webapp',
-    version='0.1',
+    version='0.0.1',
     packages=['mypy_webapp', 'mypy_webapp.tests'],
-    url='',
+    url='https://github.com/diver-gent/my_webapp.git',
     license='',
     author='townsley',
     author_email='williamwtownsleyiii@gmail.com',
