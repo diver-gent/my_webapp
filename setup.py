f048e27230dfe2abb4df1dffe1edb0c5979b4a33
townsley <williamwtownsleyiii@gmail.com>
2016-04-10 10:14:56 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='mypy_webapp',
+    version='0.1',
+    packages=['mypy_webapp', 'mypy_webapp.tests'],
+    url='',
+    license='',
+    author='townsley',
+    author_email='williamwtownsleyiii@gmail.com',
+    description='Stoopid Sample Webapp'
+)
